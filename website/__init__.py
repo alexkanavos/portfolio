0239f29117d77395dd1e0108a1a6323719123654
Alex Kanavos <alexkanv@gmail.com>
2024-03-14 19:53:25 +0100

    Add home page entry content.

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from datetime import datetime
+import os
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "ALLAKSE ME !!!!"
+
+    app.jinja_env.globals.update(
+        current_year=datetime.now().year, my_name=os.getenv("MY_NAME")
+    )
 
     from .views import views
 
