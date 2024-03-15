3d184cc1c05191077dfe5dc60edee19cd0b39e0a
Alex Kanavos <alexkanv@gmail.com>
2024-03-15 12:40:06 +0100

    Set environment variables to deploy.

--- website/__init__.py
+++ website/__init__.py
@@ -5,11 +5,13 @@ import os
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "ALLAKSE ME !!!!"
+    app.config["SECRET_KEY"] = os.getenv("PORTFOLIO_FLASK_SECRET_KEY")
     app.url_map.strict_slashes = False
 
     app.jinja_env.globals.update(
-        current_year=datetime.now().year, my_name=os.getenv("MY_NAME")
+        current_year=datetime.now().year,
+        my_name=os.getenv("MY_NAME"),
+        my_email=os.getenv("MY_EMAIL"),
     )
 
     from .views import views
